19d1109533bde7fc35d91631a33173e77a8f039f
Morris Ballenger <morris.ballenger@yahoo.com>
2016-02-10 10:48:43 -0600

    added everything to make project work

---
+++ src/com/morrisballenger/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.morrisballenger; 
+
+public class HelloWorld {
+	private String message;
+	
+	public void setMessage(String message){
+		this.message = message;
+	}
+	
+	public void getMessage(){
+		System.out.println("Message: " + message);
+	}
+
+}
